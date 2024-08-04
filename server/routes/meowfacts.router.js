8124ac51362f7991ea7e5e2c99e75e8ecd36677e
slindsey9150 <stevie.lindsey.jr@gmail.com>
2024-08-04 15:49:15 -0500

    created backend logic for querying meowfacts api and tested with postman

---
+++ server/routes/meowfacts.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const axios = require('axios')
+
+
+router.get('/', (req, res) => {
+    axios.get('https://meowfacts.herokuapp.com/')
+    .then((response) => {
+        res.send(response.data);
+    })
+    .catch(error => {
+        console.log("error in the server", error);
+    })
+  });
+
+  module.exports = router;
